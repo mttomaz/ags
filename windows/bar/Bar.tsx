9643167a3da6c55ae9ea6f2e1f65b692ef287e78
mttomaz <mttomaz@duck.com>
2025-08-19 22:25:50 -0300

    fix hyprland workspaces

--- windows/bar/Bar.tsx
+++ windows/bar/Bar.tsx
@@ -116,14 +116,22 @@ function MediaModule() {
 
 function Workspaces() {
   const hypr = AstalHyprland.get_default()
-  const workspaces = createBinding(hypr, "workspaces")
+  const [wsList, setWsList] = createState(hypr.get_workspaces())
+
+  // INFO: https://github.com/Aylur/astal/issues/284
+  hypr.connect("workspace-added", (_, ws) => {
+    setWsList((w) => [...w, ws])
+  })
+  hypr.connect("workspace-removed", (_, wsId) => {
+    setWsList((w) => w.filter((w) => w.id != wsId))
+  })
 
   const sorted = (arr: Array<AstalHyprland.Workspace>) => {
     return arr.filter(ws => !(ws.id >= -99 && ws.id <= -2)).sort((a, b) => a.id - b.id)
   }
 
   return <box class="Workspaces">
-    <For each={workspaces(sorted)}>
+    <For each={wsList(sorted)}>
       {(ws: AstalHyprland.Workspace) => (
         <button
           class={createBinding(hypr, "focusedWorkspace").as(fw => ws === fw ? "focused" : "")}
